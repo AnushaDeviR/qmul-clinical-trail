3e723a277da4517346d3be89ee4856b7e347f7db
AnushaDeviR <anusha3599@gmail.com>
2023-10-16 10:31:58 +0100

    Server setup with Springboot

---
+++ XYZClinicalTrailServer/src/main/java/com/qmul/XYZClinicalTrailServer/XyzClinicalTrailServerApplication.java
@@ -0,0 +1,13 @@
+package com.qmul.XYZClinicalTrailServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzClinicalTrailServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzClinicalTrailServerApplication.class, args);
+	}
+
+}
