8cb522c9094dd5adea5e948a7187089fc200d0af
AnushaDeviR <anusha3599@gmail.com>
2023-10-16 12:14:16 +0100

    added CRUD endpoint functionalities

--- XYZClinicalTrailServer/src/main/java/com/qmul/XYZClinicalTrailServer/controller/TrailController.java
+++ XYZClinicalTrailServer/src/main/java/com/qmul/XYZClinicalTrailServer/controller/TrailController.java
@@ -3,12 +3,17 @@ package com.qmul.XYZClinicalTrailServer.controller;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qmul.XYZClinicalTrailServer.model.Trail;
 import com.qmul.XYZClinicalTrailServer.repo.TrailRepo;
 
 @RestController
@@ -21,25 +26,74 @@ public class TrailController {
 
     // API endpoint for Create
     @PostMapping("/addPatient")
-    public void addPatient() {
+    public ResponseEntity<Trail> addPatient(@RequestBody Trail trail) {
+        try {
+            Trail patientObj = trailRepo.save(trail);
+            return new ResponseEntity<>(patientObj, HttpStatus.OK);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     // API endpoint for Read
     @GetMapping("/getAllPatientDetails")
-    public void getAllPatientDetails() {
+    public ResponseEntity<List<Trail>> getAllPatientDetails() {
+        try {
+            List<Trail> patientList = new ArrayList<>();
+            trailRepo.findAll().forEach(patientList::add);
 
+            if (patientList.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(patientList, HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    // API endpoint for Update
-    @PutMapping("/getPatientById/{id}")
-    public void getPatientById() {
+    // API endpoint for update
+    @PutMapping("/updatePatientById/{id}")
+    public ResponseEntity<Trail> updatePatientById(@PathVariable Long id, @RequestBody Trail newPatientData) {
+        try {
+            Optional<Trail> oldPatientData = trailRepo.findById(id);
 
+            if (oldPatientData.isPresent()) {
+                Trail updatePatientData = oldPatientData.get();
+                updatePatientData.setName(newPatientData.getName());
+                updatePatientData.setAge(newPatientData.getAge());
+                updatePatientData.setGender(newPatientData.getGender());
+                updatePatientData.setCondition(newPatientData.getCondition());
+                updatePatientData.setRecruitmentDate(newPatientData.getRecruitmentDate());
+                updatePatientData.setLocation(newPatientData.getLocation());
+                updatePatientData.setRegisteredGP(newPatientData.getRegisteredGP());
+                updatePatientData.setPhoneNumber(newPatientData.getPhoneNumber());
+
+                Trail patientObj = trailRepo.save(updatePatientData);
+
+                return new ResponseEntity<>(patientObj, HttpStatus.OK);
+            }
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     // API endpoint for Delete
     @DeleteMapping("/deletePatientById/{id}")
-    public void deletePatientById() {
-
+    public ResponseEntity<HttpStatus> deletePatientById(@PathVariable Long id) {
+        try {
+            trailRepo.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- XYZClinicalTrailServer/src/main/java/com/qmul/XYZClinicalTrailServer/model/Trail.java
+++ XYZClinicalTrailServer/src/main/java/com/qmul/XYZClinicalTrailServer/model/Trail.java
@@ -2,9 +2,24 @@ package com.qmul.XYZClinicalTrailServer.model;
 
 import java.util.*;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "Patients")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 
 public class Trail {
 
@@ -16,9 +31,34 @@ public class Trail {
     private Integer age;
     private String gender;
     private String condition;
+
     private Date recruitment_date;
     private String location;
     private String registered_gp;
     private String phone_number;
+
+    public Date getRecruitmentDate() {
+        return recruitment_date;
+    }
+
+    public void setRecruitmentDate(Date recruitment_date) {
+        this.recruitment_date = recruitment_date;
+    }
+
+    public String getRegisteredGP() {
+        return registered_gp;
+    }
+
+    public void setRegisteredGP(String registered_gp) {
+        this.registered_gp = registered_gp;
+    }
+
+    public String getPhoneNumber() {
+        return phone_number;
+    }
+
+    public void setPhoneNumber(String phone_number) {
+        this.phone_number = phone_number;
+    }
 
 }
